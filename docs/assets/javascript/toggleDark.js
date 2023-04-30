4e5ca30a60b65e9d27ce7dbac0d56f10431972e4
Evan Simkowitz <esimkowitz@wustl.edu>
2023-04-30 14:40:11 -0700

    update license

--- docs/assets/javascript/toggleDark.js
+++ docs/assets/javascript/toggleDark.js
@@ -1,12 +1,3 @@
-var checkbox = document.querySelector('input[name=mode]')
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark')
-        var moon = document.getElementById('moon')
-        moon.innerHTML = feather.icons['sun'].toSvg({'fill': 'var(--fg-color-dark)'})
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light')
-        var moon = document.getElementById('moon')
-        moon.innerHTML = feather.icons['moon'].toSvg({'fill': 'var(--fg-color-dark)'})
-    }
-})
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    document.documentElement.setAttribute('data-theme', event.matches ? 'dark' : 'light');
+});
