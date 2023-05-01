651745e9ac6a0d38a88334be1ff8c0316a503a33
Evan Simkowitz <esimkowitz@wustl.edu>
2023-04-30 18:10:54 -0700

    Make dark mode responsive via CSS

--- docs/assets/javascript/toggleDark.js
+++
@@ -1,3 +0,0 @@
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    document.documentElement.setAttribute('data-theme', event.matches ? 'dark' : 'light');
-});
